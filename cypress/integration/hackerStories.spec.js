c63999ebf75b71f5fedd7a111b9d1d0e07dd98f5
Felipe Accyole <accyole@hotmail.com>
2022-07-11 23:22:07 -0300

    Utilizando o cy.getLocalStorage

--- cypress/integration/hackerStories.spec.js
+++ cypress/integration/hackerStories.spec.js
@@ -29,7 +29,7 @@ describe('Hacker Stories', () => {
 
       cy.get('.item').should('have.length', 20);
 
-      cy.contains('More').click();
+      cy.contains('More').should('be.visible').click();
 
       // cy.assertLoadingIsShownAndHidden()
       cy.wait('@getMoreStories');
@@ -39,11 +39,13 @@ describe('Hacker Stories', () => {
 
     it('searches via the last searched term', () => {
       cy.intercept('GET', `**/search?query=${newTerm}&page=0`).as('getNewTermStories');
-      cy.get('#search').clear()
+      cy.get('#search').should('be.visible').clear()
         .type(`${newTerm}{enter}`);
 
       // cy.assertLoadingIsShownAndHidden()
       cy.wait('@getNewTermStories');
+
+      cy.getLocalStorage('search').should('be.equal', newTerm);
 
       cy.get(`button:contains(${initialTerm})`)
         .should('be.visible')
@@ -51,6 +53,8 @@ describe('Hacker Stories', () => {
 
       // cy.assertLoadingIsShownAndHidden()
       cy.wait('@getStories');
+
+      cy.getLocalStorage('search').should('be.equal', initialTerm);
 
       cy.get('.item').should('have.length', 20);
       cy.get('.item')
@@ -81,10 +85,11 @@ describe('Hacker Stories', () => {
       })
     
       context('List of stories', () => {
-        it.only('shows the right data for all rendered stories', () => {
-          const stories = require('../fixtures/stories.json');
+        const stories = require('../fixtures/stories.json');
+        it('shows the right data for all rendered stories', () => {
 
           cy.get('.item').eq(0)
+            .should('be.visible')
             .should('contain', stories.hits[0].title)
             .and('contain', stories.hits[0].author)
             .and('contain', stories.hits[0].num_comments)
@@ -93,6 +98,7 @@ describe('Hacker Stories', () => {
             .should('have.attr', 'href', stories.hits[0].url);
 
           cy.get('.item').last()
+            .should('be.visible')
             .should('contain', stories.hits[1].title)
             .and('contain', stories.hits[1].author)
             .and('contain', stories.hits[1].num_comments)
@@ -102,32 +108,81 @@ describe('Hacker Stories', () => {
         });
     
         it('show one les story after dimissing the first story', () => {
-          cy.get('.button-small')
+          cy.get('.button-small').should('be.visible')
             .first()
             .click()
     
           cy.get('.item').should('have.length', 1)
         })
     
-        // Since the API is external,
-        // I can't control what it will provide to the frontend,
-        // and so, how can I test ordering?
-        // This is why these tests are being skipped.
-        // TODO: Find a way to test them out.
-        context.skip('Order by', () => {
+        
+        context('Order by', () => {
           it('orders by title', () => {
+            cy.get('.list-header-button:contains(Title)').as('titleHeader')
+              .should('be.visible')
+              .click();
+
+            cy.get('.item').first()
+              .should('be.visible').and('contain', stories.hits[0].title);
+
+            cy.get(`.item a:contains(${stories.hits[0].title})`)
+            .should('have.attr', 'href', stories.hits[0].url);
+
+            cy.get('@titleHeader')
+              .click();
+
+            cy.get('.item').first()
+            .should('be.visible').and('contain', stories.hits[1].title);
+
+            cy.get(`.item a:contains(${stories.hits[1].title})`)
+            .should('have.attr', 'href', stories.hits[1].url);
 
           });
     
           it('orders by author', () => {
+            cy.get('.list-header-button:contains(Author)').as('authorHeader')
+              .should('be.visible')
+              .click();
 
+            cy.get('.item').first()
+              .should('be.visible').and('contain', stories.hits[0].author);
+
+            cy.get('@authorHeader')
+              .click();
+
+            cy.get('.item').first()
+            .should('be.visible').and('contain', stories.hits[1].author);
           });
     
           it('orders by comments', () => {
+            cy.get('.list-header-button:contains(Comments)').as('commentsHeader')
+              .should('be.visible')
+              .click();
+
+            cy.get('.item').first()
+              .should('be.visible').and('contain', stories.hits[1].num_comments);
+
+            cy.get('@commentsHeader')
+              .click();
+
+            cy.get('.item').first()
+            .should('be.visible').and('contain', stories.hits[0].num_comments);
 
           });
     
           it('orders by points', () => {
+            cy.get('.list-header-button:contains(Points)').as('pointsHeader')
+              .should('be.visible')
+              .click();
+
+            cy.get('.item').first()
+              .should('be.visible').and('contain', stories.hits[1].points);
+
+            cy.get('@pointsHeader')
+              .click();
+
+            cy.get('.item').first()
+            .should('be.visible').and('contain', stories.hits[0].points);
 
           });
         });
@@ -153,15 +208,24 @@ describe('Hacker Stories', () => {
         cy.wait('@getEmptyStories');
 
         cy.get('#search')
+          .should('be.visible')
           .clear();
-        });
+      });
+
+      it('shows no story when none is returned', () => {
+        cy.get('.item').should('not.exist');
+
+      });
     
       it('types and hits ENTER', () => {
         cy.get('#search')
+          .should('be.visible')
           .type(`${newTerm}{enter}`)
   
         // cy.assertLoadingIsShownAndHidden()
         cy.wait('@getNewTermStories');
+
+        cy.getLocalStorage('search').should('be.equal', newTerm);
   
         cy.get('.item').should('have.length', 2)
         // cy.get('.item')
@@ -173,12 +237,15 @@ describe('Hacker Stories', () => {
   
       it('types and clicks the submit button', () => {
         cy.get('#search')
+          .should('be.visible')
           .type(newTerm)
         cy.contains('Submit')
           .click()
   
         // cy.assertLoadingIsShownAndHidden()
         cy.wait('@getNewTermStories');
+
+        cy.getLocalStorage('search').should('be.equal', newTerm);
   
         cy.get('.item').should('have.length', 2)
         // cy.get('.item')
@@ -205,17 +272,20 @@ describe('Hacker Stories', () => {
           cy.intercept('GET', `**/search**`, {fixture: 'empty'}).as('getRandomStories')
   
           Cypress._.times(6, () => {
+            const randomWord = faker.random.word();
             cy.get('#search')
               .clear()
-              .type(`${faker.random.word()}{enter}`)
+              .type(`${randomWord}{enter}`);
   
             cy.wait('@getRandomStories');
+            cy.getLocalStorage('search').should('be.equal', randomWord);
           })
   
           // cy.assertLoadingIsShownAndHidden()
   
-          cy.get('.last-searches button')
-            .should('have.length', 5)
+          cy.get('.last-searches').within(() => {
+            cy.get('button').should('have.length', 5)
+          })
         })
       })
     });
